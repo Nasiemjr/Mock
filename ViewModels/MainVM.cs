b757e136ddcfee21ba079577d9dd5a587e27b491
Nasiemjr <Nasiem1610@gmail.com>
2024-02-03 01:24:43 +0200

    Added a "Settings" page.

--- ViewModels/MainVM.cs
+++ ViewModels/MainVM.cs
@@ -4,7 +4,20 @@
     {
         public MainVM()
         {
+            var settingsVM =  new SettingsVM();
 
+            CurrentViewModel = settingsVM;
         }
+
+        public BaseVM CurrentViewModel
+        {
+            get => _currentViewModel;
+            set
+            {
+                SetProperty(ref _currentViewModel, value);
+            }
+        }
+
+        private BaseVM _currentViewModel;
     }
 }
---
+++ ViewModels/SettingsVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mock.ViewModels
+{
+    public class SettingsVM : BaseVM
+    {
+        public SettingsVM()
+        {
+            
+        }
+    }
+}
